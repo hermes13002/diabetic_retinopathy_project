cf3f3a354282d029b3c7639ff2410cc7d8df9800
TolaniSilas <tolanisilas3606@gmail.com>
2024-06-15 19:47:03 +0100

    wep app files updates

--- web_app.py
+++ web_app.py
@@ -11,29 +11,41 @@ from tensorflow.keras.models import load_model
 
 
 
-# Title and Description
+
+# ==================== Configure Streamlit page ==================== #
+
+st.set_page_config(
+    page_title="SA App",
+    page_icon=":bar_chart:",
+    layout="wide",
+)
+
+
+# ==================== Styling ==================== #
+
+
 st.markdown(
     """
     <style>
         .title-text {
-            font-size: 26px;
-            font-weight: bold;
-            color: #A52A2A;
-            margin-bottom: 20px;
+            background: linear-gradient(to right, #ff69b4, #9400d3); /* Vibrant pink/purple gradient */
+            color: white; /* White text for the header */
+            padding: 25px; /* Increased padding for more space */
+            text-align: center;
+            border-radius: 10px; /* Rounded corners for a softer look */
+            box-shadow: 2px 2px 5px rgba(0,0,0,0.1); /* Subtle shadow for depth */
         }
-        .description-text {
-            font-size: 16px;
-            color: #FFE4B5;
-            margin-bottom: 30px;
-            padding: 15px;
-            background-color: #333333;
-            border: 1px solid black;
+        
+        .title-text h1{
+            font-size: 3rem; /* Larger font size for the header */
         }
+        
         .stApp {
             background-color: #CCCCCC; 
             padding: 10px;
             margin: 15px
         }
+        
         .footer{
             background-color: #A52A2A;
             color: #FFFFFF;
@@ -44,23 +56,148 @@ st.markdown(
             width: 100%;
             height: 50px;
         }
+        
     </style>
     """,
     unsafe_allow_html=True
-)
+    )
 
 
 
-st.markdown("<h1 class='title-text'>Automated Detection and Grading of Diabetic Retinopathy Using Machine Learning</h1>", unsafe_allow_html=True)
+# ==================== Header ==================== #
+
+st.markdown("""
+            <div class="title-text">
+            <h1>Automated Detection and Grading of Diabetic Retinopathy Using Machine Learning</h1>
+            </div>
+            """, 
+            unsafe_allow_html=True
+            )
+
+
+
+
 st.markdown(
-    """<p class='description-text'>The existing methods for detecting and grading Diabetic Retinopathy (DR) often rely on subjective assessments and extensive manual labor, leading to 
-    inefficiencies and potential inconsistencies in diagnosis. The increasing prevalence of diabetes and the limited availability of ophthalmologists further exacerbate the challenges in 
-    timely screening and diagnosis. Therefore, there is a need to develop a robust and reliable automated system that can accurately detect and grade diabetic retinopathy, enabling early 
-    intervention and personalized treatment plans. This project aims to address this challenges by leveraging machine learning and computer vision techniques. The proposed system will take 
-    fundus images of the retina as input and automatically detect the presence of diabetic retinopathy. The system will also grade the severity of the condition, providing clinicians with 
-    valuable information for treatment planning.</p>""",
-    unsafe_allow_html=True
+    """
+    <style>
+        .about-text, .get-started-section {
+            # background-color: #fff; /* White background */
+            border: 1px solid #ddd; /* Subtle border */
+            padding: 25px; /* More padding for better readability */
+            border-radius: 10px; /* Rounded corners for a softer look */
+            box-shadow: 2px 2px 5px rgba(0,0,0,0.1); /* Subtle shadow for depth */
+        }
+
+        .about-text h2, .get-started-section h3 {
+            color: #fffff;
+            margin-bottom: 15px;
+        }
+
+        .about-text p, .get-started-section p {
+            line-height: 1.5;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
 )
+
+
+
+
+# ==================== About the App Section =================== #
+
+
+with st.container():
+    
+    with st.expander("About the App"):
+        
+        st.markdown(
+            """
+            <div class="about-text">
+                <p> 
+                This web application automates the detection and grading of Diabetic Retinopathy (DR) using machine learning and computer vision. By analyzing retinal fundus images, it 
+                provides accurate, consistent, and efficient identification of DR severity, supporting early intervention and personalized treatment. Existing methods often rely on subjective
+                assessments and extensive manual labor, leading to inefficiencies and inconsistencies. The increasing prevalence of diabetes and the limited availability of ophthalmologists 
+                further exacerbate timely screening and diagnosis challenges. This project addresses these issues by leveraging machine learning and computer vision techniques. The model 
+                underwent fine-tuning using Convolutional Neural Network techniques, achieving an impressive accuracy and a recall score of 96%, indicating its ability to effectively capture 
+                cases of diabetic retinopathy. This enhances timely and reliable screening and diagnosis, addressing the rising diabetes prevalence and ophthalmologist shortage. 
+                </p>            
+            </div>
+            """,
+            unsafe_allow_html=True
+            )
+        
+        
+        
+
+# ==================== Get Started Section =================== #
+
+with st.container():
+    st.markdown(
+        """
+        <div class="get-started-section">
+            <h3>Get Started</h3>
+            <p>
+            Just upload your retinal fundus image, and the system will quickly analyze it to detect and grade the presence of Diabetic Retinopathy (DR) along with the percentage level of 
+            confidence.
+            </p>
+        </div>
+        """,
+        unsafe_allow_html=True
+        )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ==================== Image Input Section ==================== #
 
 
 # File Uploader
@@ -69,8 +206,10 @@ uploaded_image = st.file_uploader("Upload an image:", type=["jpg", "jpeg"])
 # Display Uploaded Image and Classification Results
 if uploaded_image is not None:
     if uploaded_image.type in ["image/jpg", "image/jpeg"]:
+        
         # Display loading spinner
         with st.spinner('Classifying...'):
+            
             image = Image.open(uploaded_image)
             st.image(image, caption="Uploaded Image")
             
@@ -191,50 +330,50 @@ st.markdown(
 )
 
 
-# Define the sidebar content using Streamlit's layout components.
-st.sidebar.markdown(
-    """
-    <div class="info">
-        <h1 class="h1">About</h1>
-        <p>
-        This web application automates the detection and grading of Diabetic Retinopathy (DR) using machine learning and computer vision. By analyzing retinal fundus images, it aims to provide
-        accurate, consistent, and efficient identification of DR severity, supporting early intervention and personalized treatment. This addresses the rising diabetes prevalence and 
-        ophthalmologist shortage, enhancing timely and reliable screening and diagnosis.
-        </p>
-        <h2 class="header">Purpose</h2>
-        <p>
-        The purpose of this project is to demonstrate the capabilities of machine learning in image classification and to provide a practical example of how machine learning can be applied 
-        to real-world problems.
-        </p>
-        <h2 class="header">Contributors</h2>
-        <p>
-        <a href="https://github.com/TolaniSilas" target="_blank">Osunba Silas Tolani</a>
-        </p>
-        <p>
-        <a href="https://github.com/TolaniSilas" target="_blank">Soares Ayoigbala</a>
-        </p>
-        <p>
-        <a href="https://github.com/TolaniSilas" target="_blank">Ogundipe Elijah</a>
-        </p>        
-        <h2 class="header">Acknowledgments</h2>
-        <p>I'd like to thank the Streamlit community for their support and the developers of the underlying libraries used in this project.</p>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+# # Define the sidebar content using Streamlit's layout components.
+# st.sidebar.markdown(
+#     """
+#     <div class="info">
+#         <h1 class="h1">About</h1>
+#         <p>
+#         This web application automates the detection and grading of Diabetic Retinopathy (DR) using machine learning and computer vision. By analyzing retinal fundus images, it aims to provide
+#         accurate, consistent, and efficient identification of DR severity, supporting early intervention and personalized treatment. This addresses the rising diabetes prevalence and 
+#         ophthalmologist shortage, enhancing timely and reliable screening and diagnosis.
+#         </p>
+#         <h2 class="header">Purpose</h2>
+#         <p>
+#         The purpose of this project is to demonstrate the capabilities of machine learning in image classification and to provide a practical example of how machine learning can be applied 
+#         to real-world problems.
+#         </p>
+#         <h2 class="header">Contributors</h2>
+#         <p>
+#         <a href="https://github.com/TolaniSilas" target="_blank">Osunba Silas Tolani</a>
+#         </p>
+#         <p>
+#         <a href="https://github.com/TolaniSilas" target="_blank">Soares Ayoigbala</a>
+#         </p>
+#         <p>
+#         <a href="https://github.com/TolaniSilas" target="_blank">Ogundipe Elijah</a>
+#         </p>        
+#         <h2 class="header">Acknowledgments</h2>
+#         <p>I'd like to thank the Streamlit community for their support and the developers of the underlying libraries used in this project.</p>
+#     </div>
+#     """,
+#     unsafe_allow_html=True
+# )
 
 
 
-st.sidebar.markdown("""
-    <div class="feedback">
-        <h1 class="header">Feedback</h1>
-    </div>
-""", unsafe_allow_html=True)
+# st.sidebar.markdown("""
+#     <div class="feedback">
+#         <h1 class="header">Feedback</h1>
+#     </div>
+# """, unsafe_allow_html=True)
 
-feedback = st.sidebar.text_area("Please share your feedback here:", max_chars=500)
+# feedback = st.sidebar.text_area("Please share your feedback here:", max_chars=500)
 
-if st.sidebar.button("Submit"):
-    # Process the feedback (you can save it to a database or send it via email)
-    st.sidebar.success("Thank you for your feedback! It has been submitted successfully.")
+# if st.sidebar.button("Submit"):
+#     # Process the feedback (you can save it to a database or send it via email)
+#     st.sidebar.success("Thank you for your feedback! It has been submitted successfully.")
 
 
